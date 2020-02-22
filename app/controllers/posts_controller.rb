effdbef5ab81ab85bbf2d087c98ff80f19010126
Alina Kornilova <alinakornilova78@gmail.com>
2020-02-22 19:20:11 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,11 +18,11 @@ class PostsController < ApplicationController
 
  def new
   if params[:author_id] && !Author.exists?(params[:author_id])
-    redirect_to authors_path, alert: "Author not found."
-  else
-    @post = Post.new(author_id: params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = Post.new(author_id: params[:author_id])
+    end
   end
-end
 
   def create
     @post = Post.new(post_params)
@@ -38,16 +38,16 @@ end
 
   def edit
   if params[:author_id]
-    author = Author.find_by(id: params[:author_id])
-    if author.nil?
-      redirect_to authors_path, alert: "Author not found."
+      author = Author.find_by(id: params[:author_id])
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found."
+      else
+        @post = author.posts.find_by(id: params[:id])
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      end
     else
-      @post = author.posts.find_by(id: params[:id])
-      redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      @post = Post.find(params[:id])
     end
-  else
-    @post = Post.find(params[:id])
-  end
 end
 
   private
