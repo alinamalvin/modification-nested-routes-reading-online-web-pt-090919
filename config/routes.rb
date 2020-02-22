b7043286fd21df84217c8ad596f7dae833626c76
Alina Kornilova <alinakornilova78@gmail.com>
2020-02-22 18:56:07 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
-Rails.application.routes.draw do
-  resources :authors, only: [:show, :index] do
-  resources :posts, only: [:show, :index, :new]
-end
-resources :posts
+resources :authors, only: [:show, :index] do
+  resources :posts, only: [:show, :index, :new, :edit]
 end
